a2ff6db1a9ab6c5535d4da6a35fa78efbf3035bb
Sambudi Sugya Putra <sambudisp@gmail.com>
2026-04-17 09:13:41 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.gps_location_tracker_sentinel_tech"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" //flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gps_location_tracker_sentinel_tech"
-        minSdk = flutter.minSdkVersion
+        minSdk = "23" //flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
